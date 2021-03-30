c558c654de9a4baaecf867ca83f7b2c3a8ad0226
Naman jain <namanjain.it19@jecrc.ac.in>
2021-03-30 19:14:15 +0530

    Update AzureExampleApplication.java

--- src/main/java/com/example/AzureExampleApplication.java
+++ src/main/java/com/example/AzureExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzureExampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello, World. This is a URL Status checker.");
 		SpringApplication.run(AzureExampleApplication.class, args);
 	}
 
