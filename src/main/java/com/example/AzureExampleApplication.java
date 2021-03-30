b68fad8c72b3c15615b452f38377a5cd76e83338
Naman jain <namanjain63@yahoo.com>
2021-03-30 17:33:36 +0530

    First Commit

---
+++ src/main/java/com/example/AzureExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureExampleApplication.class, args);
+	}
+
+}
